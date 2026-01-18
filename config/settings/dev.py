0d18263de7c881c1edae08fdaebf9e5995c74cbe
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-01-18 18:33:22 +0500

    initial commit

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,17 +1,18 @@
-# from .base import BASE_DIR
-from .base import *
+import os
+import dj_database_url
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
