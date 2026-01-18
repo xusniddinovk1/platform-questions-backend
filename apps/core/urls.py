5c18c4ce2e9c40238ddf25e3d125c84ed98cfb4e
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-01-18 17:57:07 +0500

    update uls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from apps.questions.views import QuestionListView, QuestionDetailVIew, AnswerCreateView
 from apps.core.views import HealthCheckView
 
 urlpatterns = [
     path("health", HealthCheckView.as_view()),
+    path('questions/', QuestionListView.as_view(), name='question_list'),
+    path('questions/<int:pk>/', QuestionDetailVIew.as_view(), name='question_detail'),
+    path('answers/', AnswerCreateView.as_view(), name='answer_create'),
 ]
--- apps/questions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import QuestionListView, QuestionDetailVIew, AnswerCreateView
-
-urlpatterns = [
-    path('questions/', QuestionListView.as_view(), name='question_list'),
-    path('questions/<int:pk>/', QuestionDetailVIew.as_view(), name='question_detail'),
-    path('answers/', AnswerCreateView.as_view(), name='answer_create'),
-]
