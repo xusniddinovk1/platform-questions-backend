df7b0209827a9a07770d420d5694c9941c6b8f4a
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-01-27 17:25:07 +0500

    initial commit

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,17 +1,19 @@
+import os
+import dj_database_url
 from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+#
 # DATABASES = {
-#     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+DATABASES = {
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
