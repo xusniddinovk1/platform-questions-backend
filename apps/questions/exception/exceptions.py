f7cd9c87d32a42eddddc5d479e64b17aab478957
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-02-07 12:42:39 +0500

    fix bugs

--- apps/questions/exception/exceptions.py
+++ apps/questions/exception/exceptions.py
@@ -5,24 +5,24 @@ from rest_framework import status
 
 
 class QuestionsBaseException(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
+    status_code: int = status.HTTP_400_BAD_REQUEST
     default_detail = "Questions service error."
     default_code = "questions_error"
 
 
 class PermissionDeniedForAction(QuestionsBaseException):
-    status_code = status.HTTP_403_FORBIDDEN
+    status_code: int = status.HTTP_403_FORBIDDEN
     default_detail = "You don't have permission to perform this action."
     default_code = "permission_denied"
 
 
 class QuestionNotFound(QuestionsBaseException):
-    status_code = status.HTTP_404_NOT_FOUND
+    status_code: int = status.HTTP_404_NOT_FOUND
     default_detail = "Question not found."
     default_code = "question_not_found"
 
 
 class AnswerNotFound(QuestionsBaseException):
-    status_code = status.HTTP_404_NOT_FOUND
+    status_code: int = status.HTTP_404_NOT_FOUND
     default_detail = "Answer not found."
     default_code = "answer_not_found"
