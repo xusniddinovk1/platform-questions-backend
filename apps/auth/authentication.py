0d29af00f6fbc864604bf456d6e4769a3b869c20
Freyzan2006 <freyzan2006@gmail.com>
2026-02-28 16:54:24 +0500

    fix: try exept gruid auth

--- apps/auth/authentication.py
+++ apps/auth/authentication.py
@@ -4,6 +4,7 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.request import Request
 
 from apps.auth.container import get_auth_service
+from apps.core.responses import build_error_response
 from apps.user.models import User
 
 auth_service = get_auth_service()
@@ -16,7 +17,15 @@ class CustomJWTAuthentication(BaseAuthentication):
             return None
 
         token = auth_header.split(" ")[1]
-        user = auth_service.authenticate_token(token)
+        try:
+            user = auth_service.authenticate_token(token)
+        except ValueError:
+            return build_error_response(
+                status_code=401,
+                code="ACCESS_TOKEN_REQUIRED",
+                title="Access token is required",
+                detail="Access token must be provided in the Authorization header",
+            )
 
         if user is None:
             return None
